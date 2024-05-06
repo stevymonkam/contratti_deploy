5abdcb6b0d04db0a01ea73d8702290c5028badef
stevymonkam <65792571+stevymonkam@users.noreply.github.com>
2024-05-06 22:32:03 +0200

    Update config.service.ts

--- src/app/service/config/config.service.ts
+++ src/app/service/config/config.service.ts
@@ -9,7 +9,7 @@ export class ConfigService {
 
   API_URL: string
   constructor() {
-        this.API_URL='http://localhost:8080/api';
+        this.API_URL='http://192.168.56.14:83:8080/api';
         //this.API_URL=process.env.API_URL;
         //this.API_URL='https://cors-anywhere.herokuapp.com/http://api.contratti.immobiliz.com/api'; //http://127.0.0.1:8000
   }
